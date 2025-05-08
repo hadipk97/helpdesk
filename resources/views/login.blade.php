b96f6baabd7dd3f344b2c97ebe124410944761d4
Julll <zulsham@prismakhas.com>
2025-05-08 18:15:24 +0800

    new login and register page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,66 +1,83 @@
 @include('template.header')
-<section class="content container">
-    <br><br><br><br><br>
-    <div class="row">
-        <div class="col-3"></div>
-        <div class="col-6">
-            <div class="card">
-                <div class="card-header">
-                    <div class="row">
-                        <div class="col-md-4 ">
+<section>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-6 col-12 text-black d-flex justify-content-center align-items-center">
+                <div class="">
+                    <div class="d-flex align-items-center">
 
-                        </div>
-                        <div class="col-md-12">
-                            <center><img src="/prismkhas.png" width="350px" height="100px"></center>
-                        </div>
+                        <form style="width: 28rem; height: auto;" action="{{ route('logmasuk') }}" method="post">
+                            @csrf
+                            <h2 class="mb-3 pb-3 mt-4 text-center fw-semibold" style="font-family: 'Poppins', sans-serif !important;">Log in to your account</h2>
+
+                            <div class="input-group input-group-lg mb-4">
+                                <span class="input-group-text bg-muted">
+                                    <i class="fas fa-user text-muted"></i>
+                                </span>
+                                <input type="text" id="username" name="username" class="form-control border-start-0" placeholder="Username" />
+                            </div>
+
+                            <div class="input-group input-group-lg mb-0">
+                                <span class="input-group-text bg-muted">
+                                    <i class="fas fa-lock text-muted"></i>
+                                </span>
+                                <input type="password" id="password" name="password" class="form-control border-start-0" placeholder="Password" />
+                            </div>
+                            <div class="d-flex justify-content-between align-items-center mb-5 mt-2">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" value="" id="rememberMe">
+                                    <label class="form-check-label text-muted" for="rememberMe">
+                                        Remember me
+                                    </label>
+                                </div>
+
+                                <p class="mb-0">
+                                    <a class="text-muted" href="{{ route('forgotPassword') }}">Forgot password?</a>
+                                </p>
+                            </div>
+
+                            <div class="pt-1 px-4 mb-4 d-flex justify-content-center">
+                                <button data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-lg btn-block" type="submit">Login</button>
+                            </div>
+
+
+                            <p class="text-center">Don't have an account? <a href="{{ route('register') }}" class="link-info">Register here</a></p>
+
+                        </form>
+
                     </div>
                 </div>
-                <!-- /.card-header -->
 
-                <div class="card-body col-md-10 align-self-center">
 
-                    <form action="{{ route('logmasuk') }}" method="post">
-                        @csrf
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <!-- <label for="username"> Username: </label> --><br>
-                                    <input type="text" id="username" name="username" class="form-control"
-                                        placeholder="Username" maxlength="50">
-                                </div>
-                            </div>
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <!-- <label for="password"> Password: </label> -->
-                                    <input type="password" id="password" name="password" class="form-control"
-                                        placeholder="Password">
-                                    <a href="{{ route('forgotPassword') }}"
-                                        style="font-size: 14px; color: red; font-style: italic" title="Register">Forgot
-                                        Password?</a>
-                                </div>
 
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <a href="{{ route('register') }}" type="button" class="btn btn-block btn-primary"
-                                        title="Register">Register New User</a>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <button type="submit" class="btn btn-block btn-success"
-                                        title="Login">Login</button>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-                <!-- /.card-body -->
             </div>
-            <!-- /.card -->
+            <div class="col-lg-6 col-12 rounded-10 d-flex flex-column align-items-center justify-content-center position-relative"
+                style="background-image: url({{ asset('assets/prismakhas/bg-login.png') }}); background-size: cover; background-position: center; height: 100vh;">
+                <div class="position-absolute" style="top:15px; right:15px;">
+
+                    <ul class="nav nav-pills border-0 bg-light rounded-pill px-2 py-1 shadow-sm" style="width: max-content; font-size: 0.85rem;">
+                        <li class="nav-item">
+                            <a class="nav-link {{ request()->routeIs('login') ? 'active' : '' }} px-3 py-1 rounded-pill"
+                                href="{{ route('login') }}" id="login-tab">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link {{ request()->routeIs('register') ? 'active' : '' }} px-3 py-1 rounded-pill"
+                                href="{{ route('register') }}" id="register-tab">Register</a>
+                        </li>
+                    </ul>
+
+                </div>
+                <div class="d-flex flex-column justify-content-center align-items-center mb-5 pb-5">
+                    <img src="{{ asset('assets/prismakhas/logo-pk-white.png') }}" class="" alt="" width="37%">
+                    <div class="text-white text-center mt-5">
+                        <h1 class="fw-bold" style="font-family: 'Poppins', sans-serif !important; letter-spacing: 1px">WELCOME TO</h1>
+                        <h1 class="fw-bold" style="font-family: 'Poppins', sans-serif !important; letter-spacing: 1px">HELPDESK TICKETING SYSTEM</h1>
+                    </div>
+                </div>
+
+
+            </div>
         </div>
-        <!-- /.col -->
     </div>
-    <!-- /.row -->
 </section>
 @include('template.footer');
